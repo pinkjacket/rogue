d0c7e7ba2ca6b32d729b00905042c1ab6e110f2d
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2020-02-17 01:52:15 -0800

    bullets now have impact particle effect

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -6,6 +6,7 @@ public class PlayerBullet : MonoBehaviour
 {
     public float speed = 7.5f;
     public Rigidbody2D bulRB;
+    public GameObject impactEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,11 @@ public class PlayerBullet : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible() {
         Destroy(gameObject);
     }
 }
