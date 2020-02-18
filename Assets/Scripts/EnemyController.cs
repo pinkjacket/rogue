9b74888ac4381878b29e91d23a7ca1c948f64acc
pinkjacket <wolfex.sylcom@gmail.com>
2020-02-17 18:35:08 -0800

    enemy now exists, chases player when within range

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody2D enRB;
+    public float moveSpeed;
+    public float attentionRange;
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, PlayerController.instance.transform.position) < attentionRange) {
+            moveDirection = PlayerController.instance.transform.position - transform.position;
+        } else {
+            moveDirection = Vector3.zero;
+        }
+        moveDirection.Normalize();
+
+        enRB.velocity = moveDirection * moveSpeed;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public static PlayerController instance;
     public float moveSpeed;
     private Vector2 moveInput;
     public Rigidbody2D playerRB;
@@ -14,6 +16,10 @@ public class PlayerController : MonoBehaviour
     public Transform firePoint;
     public float shotDelay;
     private float shotDelayCounter;
+
+    private void Awake() {
+        instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
