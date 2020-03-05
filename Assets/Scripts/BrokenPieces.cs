eb8369a3fc24c2322e1698b368ae0082783fc301
pinkjacket <wolfex.sylcom@gmail.com>
2020-03-04 20:12:38 -0800

    improved box debris

---
+++ Assets/Scripts/BrokenPieces.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenPieces : MonoBehaviour
+{
+    public float pieceSpeed = 3f;
+    private Vector3 pieceDirection;
+    public float pieceSlowDown = 5f;
+    public float lifetime = 3f;
+    public SpriteRenderer theRenderer;
+    public float fadeOut = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pieceDirection.x = Random.Range(-pieceSpeed, pieceSpeed);
+        pieceDirection.y = Random.Range(-pieceSpeed, pieceSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += pieceDirection * Time.deltaTime;
+
+        pieceDirection = Vector3.Lerp(pieceDirection, Vector3.zero, pieceSlowDown * Time.deltaTime);
+
+        lifetime -= Time.deltaTime;
+        if(lifetime < 0) {
+            theRenderer.color = new Color(theRenderer.color.r, theRenderer.color.g, theRenderer.color.b, Mathf.MoveTowards(theRenderer.color.a, 0f, fadeOut * Time.deltaTime));
+            if(theRenderer.color.a == 0f) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
