c2dbf566d6b797fa9c9dc606aaa143dc0f4b583a
pinkjacket <wolfex.sylcom@gmail.com>
2020-03-05 22:25:50 -0800

    items now sort automatically onto layers

---
+++ Assets/Scripts/SpriteSorter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSorter : MonoBehaviour
+{
+    private SpriteRenderer theRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRenderer = GetComponent<SpriteRenderer>();
+
+        theRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y * -10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
