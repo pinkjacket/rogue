74d2a71181dcddc66afdfcd91ff1a5ba62388ad3
pinkjacket <wolfex.sylcom@gmail.com>
2020-03-06 16:53:02 -0800

    object/bullet interactions cleaned up

--- Assets/Scripts/Breakables.cs
+++ Assets/Scripts/Breakables.cs
@@ -19,32 +19,39 @@ public class Breakables : MonoBehaviour {
 
     }
 
+    public void BoxBreak() {
+        Destroy(gameObject);
+        AudioManager.instance.PlaySFX(0);
+
+        //show broken crate bits
+        int piecesDropped = Random.Range(1, maxPieces);
+
+        for (int i = 0; i < piecesDropped; i++) {
+            int randomPiece = Random.Range(0, BrokenPieces.Length);
+            Instantiate(BrokenPieces[randomPiece], transform.position, transform.rotation);
+        }
+
+        //drop items
+        if (itemInside) {
+            float dropChance = Random.Range(0f, 100f);
+
+            if (dropChance < dropPercent) {
+                int randomItem = Random.Range(0, possibleContents.Length);
+                Instantiate(possibleContents[randomItem], transform.position, transform.rotation);
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
             if (PlayerController.instance.dashCounter > 0) {
-                Destroy(gameObject);
-                AudioManager.instance.PlaySFX(0);
-
-                //show broken crate bits
-                int piecesDropped = Random.Range(1, maxPieces);
-
-                for(int i = 0; i < piecesDropped; i++) {
-                    int randomPiece = Random.Range(0, BrokenPieces.Length);
-                    Instantiate(BrokenPieces[randomPiece], transform.position, transform.rotation);
-                }
-
-                //drop items
-                if (itemInside) {
-                    float dropChance = Random.Range(0f, 100f);
-
-                    if(dropChance < dropPercent) {
-                        int randomItem = Random.Range(0, possibleContents.Length);
-                        Instantiate(possibleContents[randomItem], transform.position, transform.rotation);
-                    }
-                }
-                
+                BoxBreak();
             }
 
+        }
+
+        if (other.tag == "PlayerBullet") {
+            BoxBreak();
         }
 
     }
