15f427300e598add945774b6b920624169d035f2
pinkjacket <wolfex.sylcom@gmail.com>
2020-03-06 21:31:03 -0800

    Multiple rooms and camera exist

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController instance;
+    public float cameraSpeed;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null) {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), cameraSpeed * Time.deltaTime);
+        }       
+    }
+}
