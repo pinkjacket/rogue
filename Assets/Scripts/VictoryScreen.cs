dd3302441d071c683f794aeb5f5acf84218047df
pinkjacket <wolfex.sylcom@gmail.com>
2020-03-08 03:38:21 -0700

    victory screen added after second level end

---
+++ Assets/Scripts/VictoryScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryScreen : MonoBehaviour
+{
+    public float waitForInput = 2f;
+    public GameObject anyKeyText;
+    public string mainMenuScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waitForInput > 0) {
+            waitForInput -= Time.deltaTime;
+            if(waitForInput <= 0) {
+                anyKeyText.SetActive(true);
+            }
+        } else {
+            if (Input.anyKeyDown) {
+                SceneManager.LoadScene(mainMenuScene);
+            }
+        }
+    }
+}
