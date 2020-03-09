6759b06dfb8280f9fa937e46c6fc7a754a32bfb3
pinkjacket <wolfex.sylcom@gmail.com>
2020-03-08 19:00:53 -0700

    generates starting room

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    public GameObject layoutRoom;
+    public Color startColor, endColor;
+    public int distanceToExit;
+    public Transform generatorPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(layoutRoom, generatorPoint.position, generatorPoint.rotation).GetComponent<SpriteRenderer>().color = startColor;
     }
 
     // Update is called once per frame
